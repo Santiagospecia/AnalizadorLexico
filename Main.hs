8cf4a232b74a5e3553b1538d9f2a8a22a43e791a
Santiago <santiagospecia@gmail.com>
2020-07-08 14:59:15 -0300

    Lectura de archivos

--- Main.hs
+++ Main.hs
@@ -1,4 +1,14 @@
 module Main where
+import System.IO
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =  do
+    putStrLn "-----------------------------------"
+    putStrLn "      Welcome to our analyzer!     "
+    putStrLn "-----------------------------------"
+    putStrLn "Enter the file name: " 
+    fileName <- getLine    --Lee una linea de texto y se la asigna a la variable fileName (aqui ingresamos el nombre del fichero)
+    handle <- openFile fileName ReadMode --tomamos la ruta del fichero y devuelve una accion de E/S
+    contents <- hGetContents handle     -- lee el contenido
+    putStr contents
+    hClose handle --Cerramos el fichero!
