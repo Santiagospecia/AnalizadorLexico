fc746fec487489c95a989167758778ea79894459
Santiago <santiagospecia@gmail.com>
2020-07-13 17:36:45 -0300

    Creacion de tokens

--- Main.hs
+++ Main.hs
@@ -1,6 +1,49 @@
 module Main where
 import System.IO
 
+
+
+--Definimos un nuevo tipo de dato, estos van a ser nuestros tokens :
+
+data Tokens = EOF   | --Tiene éxito si estamos al final de la entrada
+              ID    String | -- nombre
+              TIPO  String | --tipo de dato
+              OPERADOR Char| -- Rerencia a los operadores aritmeticos + - * /
+              LOGICOS String | -- Referencia a los operadores logicos > < == etc.
+              DIGITO String  | -- Números
+              CADENA String  |  -- Palabras
+              CONDICIONALES String | --Operadores condicionales for, while, if, else, etc. 
+              DECLARACIONES String |
+              PROTOTIPOS String 
+              deriving (Show)  
+
+
+analizer :: String ->[Tokens]
+
+
+tipoToken :: String -> Tokens
+tipoToken "void" = TIPO "void"
+tipoToken "int" = TIPO "int"
+tipoToken "string" = TIPO "string"
+tipoToken "float" = TIPO "float"
+tipoToken "double" = TIPO "double"
+tipoToken "char" = TIPO "char"
+tipoToken "long" = TIPO "long"
+tipoToken "boolean" = TIPO "boolean"
+tipoToken "short" = TIPO "short"
+tipoToken "byte" = TIPO "byte"
+tipoToken "if" = CONDICIONALES "if"
+tipoToken "else" = CONDICIONALES "else"
+tipoToken "while" = CONDICIONALES "while"
+tipoToken "do" = CONDICIONALES "do"
+tipoToken "for" = CONDICIONALES "for"
+
+
+espacio simb = simb <= ' ' 
+
+
+
+
 main :: IO ()
 main =  do
     putStrLn "-----------------------------------"
